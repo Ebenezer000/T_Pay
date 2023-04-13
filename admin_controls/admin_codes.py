d19fc0b27c94f942c67306e87346c75a43105f06
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-04-13 22:08:40 +0100

    ds

--- admin_controls/admin_codes.py
+++ admin_controls/admin_codes.py
@@ -9,5 +9,5 @@ client = FaunaClient(secret=fauna_secret, domain="db.us.fauna.com")
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 headers["Accept"] = "application/json"
-headers["X-Client-Id"] = "<API_KEY_VALUE>"
-headers["X-Client-Secret"] = "<API_KEY_VALUE>"
+headers["X-Client-Id"] = "NAaQFC4P"
+headers["X-Client-Secret"] = "WU68WYjCX92D1Tm8oHcEG48g"
