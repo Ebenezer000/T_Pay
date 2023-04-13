40b6711b6b5677de9ba661c52bcae9bc0f53d7db
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-04-13 22:27:20 +0100

    ds

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
 from flask import Flask, request, make_response
 from faunadb import query as q
-from admin_controls.admin_codes import client, headers
+from admin_controls.admin_codes import client
 from ussd_codes.ussd_codes import replies
 import requests
-
-url = "https://deep.qrios.com/api/v1/ussd/app/push"
-
-
-data = """
-{
-  "appId": "T_pay",
-  "msisdn": "2348177642325",
-  "contextData": "devCorrelation:1234"
-}
-"""
-
-
 
 
 app = Flask(__name__)
@@ -23,13 +10,7 @@ app = Flask(__name__)
 @app.route("/ussd", methods=["GET", "POST"])
 def index():
     response = ""
-    data = """
-{
-  "appId": "T_pay",
-  "msisdn": "2348177642325",
-  "contextData": devCorrelation:1234
-}
-"""
+
     if request.method == "GET":
         response = "Hello it works wonders"
     if request.method == 'POST' :
