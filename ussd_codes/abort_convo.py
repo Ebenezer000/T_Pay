ae8df1dae2e24a57924be3e4af884882d7bcb73b
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-04-14 00:04:02 +0100

    reupdate

--- ussd_codes/abort_convo.py
+++ ussd_codes/abort_convo.py
@@ -2,9 +2,9 @@ from admin_controls.admin_codes import client, ussd_sender
 
 
 #####
-def replies():
+def replies(request):
 
-    new_data = ussd_sender.get_new_conversation
+    new_data = ussd_sender.get_new_conversation(request)
     client = new_data["client"]
     session = new_data["session"]
     mobile = new_data["mobile"]
@@ -14,4 +14,15 @@ def replies():
     signed = using["data"]["signed"]
 
     if signed == "":
-        ussd_sender.reply_user(message=f"WELCOME")
+        ussd_sender.reply_user(message=f"""WELCOME TO TPAY
+
+Please set up your TPay PIN to use this service """)
+
+    else:
+        ussd_sender.reply_user(
+            message="""WELCOME TO TPAY
+What would you like to do today
+1. Transfer 
+2. Check Balance
+3. Account Settings"""
+        )
